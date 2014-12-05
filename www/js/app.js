bf003e4c508e3c81d984fdddc4bab1a93bb27886
Santos Jiménez Linares <sjimenez77@gmail.com>
2014-12-05 07:43:31 +0000

    Using Sass

--- www/js/app.js
+++ www/js/app.js
@@ -36,11 +36,11 @@ var app = angular.module('cuaderno', [
         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
         // for form inputs)
         if(window.cordova && window.cordova.plugins.Keyboard) {
-          cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
         }
         if(window.StatusBar) {
-          // org.apache.cordova.statusbar required
-          StatusBar.styleDefault();
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
         }
     });
 })
