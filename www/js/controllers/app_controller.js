e199e5c2b7b1f767e38207e5e18bfad637b44dc6
Santos Jiménez <sjimenez77@gmail.com>
2014-12-09 17:55:35 +0100

    Alias controllers. Login validation

--- www/js/controllers/app_controller.js
+++ www/js/controllers/app_controller.js
@@ -2,14 +2,16 @@
 'use strict';
 
 appControllers.controller('AppCtrl',[
+    '$scope',
     '$window',
     '$ionicModal',
     '$timeout',
     '$ionicNavBarDelegate',
     'USER_ROLES',
     'AuthService',
-    function($window, $ionicModal, $timeout, $ionicNavBarDelegate, USER_ROLES, AuthService) {
+    function($scope, $window, $ionicModal, $timeout, $ionicNavBarDelegate, USER_ROLES, AuthService) {
         // Form data for the login modal
+        this.modal = null;
         this.loginData = {};
         
         // User
@@ -20,16 +22,68 @@ appControllers.controller('AppCtrl',[
         this.setCurrentUser = function (user) {
             this.currentUser = user;
         };
+
+        // Create the login modal that we will use later
+        $ionicModal.fromTemplateUrl('templates/login.html', {
+            scope: $scope
+        }).then(function(modal) {
+            self.modal = modal;
+        });
+
+        // Triggered in the login modal to close it
+        this.closeLogin = function() {
+            this.modal.hide();
+            this.errorMessages = [];
+        };
     
+        // Open the login modal
+        this.showLogin = function() {
+            this.modal.show();
+        };
+        
         // Open the login modal
         this.logout = function() {
             // TODO: Logout in the service
         };
-    
+
+        // Perform the login action when the user submits the login form
+        this.doLogin = function() {
+            this.errorMessages = [];
+
+            if (this.loginData.username === undefined ||
+                this.loginData.username === '' ||
+                this.loginData.password === undefined ||
+                this.loginData.password.length < 6) {
+                // Errors
+                if (this.loginData.username === undefined || this.loginData.username === '')
+                    this.errorMessages.push('El nombre de usuario no puede estar vacío');
+                if (this.loginData.password === '' || this.loginData.password === undefined)
+                    this.errorMessages.push('La contraseña no puede estar vacía');
+                if (this.loginData.password !== undefined && this.loginData.password.length < 6)
+                    this.errorMessages.push('La contraseña no puede tener menos de 6 caracteres');
+            } else {
+                // Simulate a login delay. Remove this and replace with your login
+                // code if using a login system
+                $timeout(function() {
+                    self.closeLogin();
+                }, 1000);
+            }
+
+            // TODO: Login in the service
+            console.log('Doing login', this.loginData);
+        };
+
+        this.hideLoginErrors = function() {
+            this.errorMessages = [];
+        };
+
         // Go to signup template
         this.goSignUp = function() {
             console.log('Sign up');
             $window.location.href = '#/app/signup';
         };
+
+        // Copy 'this' object to access their methods or variables
+        var self = this;
     }
 ]);
