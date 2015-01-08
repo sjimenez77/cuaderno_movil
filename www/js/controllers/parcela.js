c9686e123a29f6a9aa23190bd0d5c959db977e87
Santos Jiménez <sjimenez77@gmail.com>
2015-01-08 18:59:47 +0100

    Editada vista de parcela. Nuevas acciones en esta vista.

--- www/js/controllers/parcela.js
+++ www/js/controllers/parcela.js
@@ -3,28 +3,72 @@
 
 appControllers.controller('ParcelaCtrl', [
     '$stateParams',
+    '$location',
 	'$filter',
+	'$ionicPopup',
+	'Parcelas',
     'Collection',
-    function($stateParams, $filter, Collection) {
-    	// parcelaId param
-    	this.parcelaId = $stateParams.parcelaId;
+    function($stateParams, $location, $filter, $ionicPopup, Parcelas, Collection) {
+    	// Data for selectors
+    	// TODO: Get from server
+    	this.cultivos = Collection.cultivos;
     	// Edit mode
     	this.blocked = true;
     	// Get the item
-    	this.details = function() {
-		    var found = $filter('filter')(Collection.parcelas, { id: this.parcelaId }, true);
+    	this.details = function(parcelaId) {
+		    var found = $filter('filter')(Collection.parcelas, { id: parcelaId }, true);
 		    if (found.length) {
 		        return found[0];
 		    } else {
-		        return false;
+		        return {};
 		    }
 		};
 
-		console.log('Parcela detalle:', this.details());
+		this.data = this.details($stateParams.parcelaId);
+
+		console.log('Parcela detalle:', this.data);
 		
 		// Toggle edit mode
 		this.toggleEdit = function() {
 			this.blocked = !this.blocked;
 		};
+
+		// Remove this item
+		this.remove = function() {
+			if (this.data !== {}) {
+				// A confirm dialog
+				var confirmPopup = $ionicPopup.confirm({
+				    title: '<span class="assertive">Atención</span>',
+				    subTitle: 'Borrará la parcela y apuntes asociados',
+				    template: '¿Está seguro que quiere borrar esta parcela?',
+				    cancelText: 'No',
+				    cancelType: 'button-default',
+				    okText: 'Borrar',
+				    okType: 'button-assertive',
+				});
+
+				confirmPopup.then(function(res) {
+				    if (res) {
+
+				        // TODO: Remove from server
+				        // Parcelas.removeParcela(Session.id, Session.userRole, parcelaId)
+				        // .then(
+				        // 	function(res) {},
+				        // 	function(error) {}
+				        // );
+
+				        var index = Collection.parcelas.indexOf(self.data);
+				        Collection.parcelas.splice(index, 1);
+				        $location.path('/app/parcelas');
+
+				    } else {
+				        console.log('Remove Parcela canceled...');
+				        $ionicListDelegate.closeOptionButtons();
+				    }
+				});
+			}
+		};
+
+		var self = this;
     }
 ]);
--- www/js/services/collections.js
+++ www/js/services/collections.js
@@ -4,6 +4,24 @@
 appServices.service('Collection', function () {
     // Create app collections
     this.parcelas = [];
+    this.cultivos = [
+    	{
+    		id: '0',
+    		name: 'VID',
+    	},
+    	{
+    		id: '1',
+    		name: 'TOMATE',
+    	},
+    	{
+    		id: '2',
+    		name: 'ARROZ',
+    	},
+    	{
+    		id: '3',
+    		name: 'PATATA',
+    	},
+    ];
 
     return this;
 });
